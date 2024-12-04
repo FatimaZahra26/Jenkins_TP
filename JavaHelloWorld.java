0bdbf5c34a90938ebd2dbfe72fc65572eacd5b06
FatimaZahra26 <imanehajaj86@gmail.com>
2024-12-04 11:28:14 +0100

    After

--- JavaHelloWorld.java
+++ JavaHelloWorld.java
@@ -1,5 +1,5 @@
 public class JavaHelloWorld{
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args){
         System.out.println("hello world");
     }
 }
